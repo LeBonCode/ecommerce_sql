d2b2f8bcb7d698a36e8930172c83360ac66acecc
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-03-14 15:22:39 +0100

    optimisations de etapes + run

--- insert_etapes.sql
+++ insert_etapes.sql
@@ -1,4 +1,6 @@
 USE mydb;
-INSERT INTO `etapes` (`id`,`libelle`) VALUES (1,'Commande validée');
-INSERT INTO `etapes` (`id`,`libelle`) VALUES (2,'Commande en préparation');
-INSERT INTO `etapes` (`id`,`libelle`) VALUES (3,'Commande expédiée');
+INSERT INTO `etapes` (`id`,`libelle`) 
+VALUES 	(1,'Commande validée'),
+		(2,'Commande en préparation'),
+		(3,'Commande expédiée');
+SHOW WARNINGS;
