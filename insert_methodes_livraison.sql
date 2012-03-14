121800866c3bfe68514490c547a294217c96d2eb
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-03-14 15:27:32 +0100

    optimisations methodes_livraison + run

--- insert_methodes_livraison.sql
+++ insert_methodes_livraison.sql
@@ -1,4 +1,6 @@
 USE mydb;
-INSERT INTO `methodes_livraison` (`id`, `libelle`) VALUES (1, 'rapide');
-INSERT INTO `methodes_livraison` (`id`, `libelle`) VALUES (2, 'relais_kiala');
-INSERT INTO `methodes_livraison` (`id`, `libelle`) VALUES (3, 'prioritaire');
+INSERT INTO `methodes_livraison` (`id`, `libelle`) 
+VALUES 	(1, 'rapide'),
+		(2, 'relais_kiala'),
+		(3, 'prioritaire');
+SHOW WARNINGS;
