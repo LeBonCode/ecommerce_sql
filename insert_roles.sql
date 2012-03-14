98c225cf525572e7121bb7de5ccb97f48b4d52c1
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-03-14 15:10:26 +0100

    optimisations : adresses, cartes_credit et roles

--- insert_roles.sql
+++ insert_roles.sql
@@ -1,5 +1,5 @@
 USE mydb;
-INSERT INTO `roles` (`id`,`libelle`) VALUES (1,'utilisateur');
-SHOW WARNINGS;
-INSERT INTO `roles` (`id`,`libelle`) VALUES (2,'admin');
+INSERT INTO `roles` (`id`,`libelle`) 
+VALUES 	(1,'utilisateur'),
+		(2,'admin');
 SHOW WARNINGS;
