fe24a0a56196741514150a391b0a9c279213e19b
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-03-14 11:10:52 +0100

    methodes_livraison

---
+++ insert_methodes_livraison.sql
@@ -0,0 +1,3 @@
+INSERT INTO `methodes_livraison` (`libelle`) VALUES ('rapide');
+INSERT INTO `methodes_livraison` (`libelle`) VALUES ('relais_kiala');
+INSERT INTO `methodes_livraison` (`libelle`) VALUES ('prioritaire');
