64ac5ad40bf40d6e854170905aeaa4a65ee7f62e
Romain Durand Saint Omer <Romain@MacBook-Pro-de-Romain.local>
2012-03-14 11:22:27 +0100

    insert etapes

---
+++ insert_etapes.sql
@@ -0,0 +1,3 @@
+INSERT INTO `etapes` (`id`,`libelle`) VALUES (1,'Commande validée');
+INSERT INTO `etapes` (`id`,`libelle`) VALUES (2,'Commande en préparation');
+INSERT INTO `etapes` (`id`,`libelle`) VALUES (3,'Commande expédiée');
