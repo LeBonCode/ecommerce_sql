aae81fd3504a8e0d482aa6c16fc137cb56d13c67
romaindso <romain.dso@gmail.com>
2012-03-14 16:42:13 +0100

    refonte avec ajout des id sur unsert_produits

--- insert_produits.sql
+++ insert_produits.sql
@@ -1,102 +1,102 @@
 USE mydb;
-INSERT INTO `produits` (`libelle`,`description`,`prix`,`categories_id`) 
-VALUES 	('et,','elit erat vitae risus. Duis a mi fringilla','438','2');
-		('luctus','Etiam gravida molestie arcu. Sed eu','100','3');
-		('quis,','feugiat. Sed nec metus facilisis lorem','379','4');
-		('ligula','posuere vulputate, lacus. Cras interdum. Nunc','394','2');
-		('Nam','Nullam velit dui, semper et,','431','2');
-		('bibendum','ullamcorper. Duis cursus, diam at pretium aliquet, metus urna convallis','500','2');
-		('ipsum.','Suspendisse sagittis. Nullam vitae diam. Proin dolor. Nulla','496','3');
-		('consectetuer','cubilia Curae; Donec tincidunt. Donec vitae erat','271','3');
-		('fringilla','tortor at risus. Nunc ac sem','87','4');
-		('blandit','adipiscing lobortis risus. In mi pede, nonummy ut, molestie','342','3');
-		('Maecenas','tellus faucibus leo, in lobortis tellus justo','369','2');
-		('nec','lacus. Mauris non dui nec urna suscipit','152','3');
-		('turpis.','Proin mi. Aliquam gravida mauris ut mi.','85','3');
-		('vestibulum,','ipsum nunc id enim. Curabitur','90','4');
-		('arcu.','fringilla cursus purus. Nullam scelerisque neque sed sem','229','4');
-		('ipsum','non, luctus sit amet, faucibus ut,','493','4');
-		('Sed','Sed id risus quis diam luctus lobortis.','289','2');
-		('risus.','semper, dui lectus rutrum urna, nec','366','3');
-		('risus','ullamcorper. Duis at lacus. Quisque','427','2');
-		('egestas','quis massa. Mauris vestibulum, neque sed','203','3');
-		('ullamcorper','velit eget laoreet posuere, enim nisl elementum purus, accumsan interdum','487','3');
-		('Morbi','mattis. Cras eget nisi dictum augue malesuada malesuada. Integer','87','4');
-		('Phasellus','odio. Aliquam vulputate ullamcorper magna.','341','4');
-		('urna','velit justo nec ante. Maecenas mi','360','4');
-		('convallis','dolor. Quisque tincidunt pede ac urna. Ut tincidunt','243','3');
-		('Morbi','turpis. In condimentum. Donec at arcu. Vestibulum ante ipsum primis','367','3');
-		('purus.','risus. Donec egestas. Duis ac arcu. Nunc mauris. Morbi non','91','4');
-		('mus.','eros non enim commodo hendrerit. Donec porttitor tellus non magna.','111','2');
-		('diam.','turpis vitae purus gravida sagittis. Duis gravida. Praesent','218','2');
-		('et','sagittis lobortis mauris. Suspendisse aliquet molestie tellus. Aenean','393','3');
-		('nisl.','Suspendisse dui. Fusce diam nunc, ullamcorper','214','4');
-		('ut','Cum sociis natoque penatibus et magnis dis parturient montes,','71','3');
-		('eget','molestie pharetra nibh. Aliquam ornare, libero','163','3');
-		('est,','Proin vel arcu eu odio tristique pharetra. Quisque ac libero','342','2');
-		('eleifend','pellentesque, tellus sem mollis dui, in sodales elit erat vitae','267','3');
-		('ipsum','quam quis diam. Pellentesque habitant morbi tristique','292','2');
-		('dolor','turpis. Nulla aliquet. Proin velit. Sed malesuada augue ut lacus.','193','3');
-		('vulputate','sit amet risus. Donec egestas. Aliquam nec enim. Nunc','400','4');
-		('nec,','dictum. Phasellus in felis. Nulla tempor augue ac ipsum.','417','2');
-		('dis','ut erat. Sed nunc est, mollis non, cursus non, egestas','491','3');
-		('ornare.','est ac facilisis facilisis, magna tellus faucibus','42','4');
-		('lacus.','at, velit. Cras lorem lorem,','46','4');
-		('lacus.','est. Nunc ullamcorper, velit in aliquet lobortis,','400','4');
-		('aliquam','dolor, tempus non, lacinia at, iaculis quis, pede. Praesent eu','97','4');
-		('dictum','eu nibh vulputate mauris sagittis placerat. Cras dictum ultricies','365','3');
-		('vel','lobortis mauris. Suspendisse aliquet molestie tellus. Aenean','391','3');
-		('ipsum','in aliquet lobortis, nisi nibh','350','2');
-		('Nam','faucibus. Morbi vehicula. Pellentesque tincidunt tempus risus. Donec egestas.','413','4');
-		('Ut','risus. Donec egestas. Duis ac arcu.','357','2');
-		('enim.','ac mattis ornare, lectus ante dictum mi, ac mattis','394','4');
-		('sapien.','mus. Donec dignissim magna a tortor.','92','2');
-		('Phasellus','sed leo. Cras vehicula aliquet libero. Integer in magna. Phasellus','361','3');
-		('lorem','tempus, lorem fringilla ornare placerat, orci lacus vestibulum lorem,','240','3');
-		('Lorem','Nulla facilisi. Sed neque. Sed','37','4');
-		('accumsan','nisi. Aenean eget metus. In','144','4');
-		('nunc','Integer id magna et ipsum cursus vestibulum.','25','2');
-		('enim.','et magnis dis parturient montes, nascetur ridiculus mus.','114','4');
-		('Duis','quam. Curabitur vel lectus. Cum sociis natoque','491','2');
-		('iaculis','libero. Proin mi. Aliquam gravida mauris ut mi. Duis','481','2');
-		('molestie','taciti sociosqu ad litora torquent per conubia nostra, per','348','4');
-		('lorem','neque sed sem egestas blandit. Nam nulla magna, malesuada','115','4');
-		('sapien.','erat volutpat. Nulla dignissim. Maecenas ornare','105','3');
-		('id,','sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum','59','4');
-		('egestas.','dignissim. Maecenas ornare egestas ligula. Nullam feugiat placerat','269','3');
-		('non,','est. Mauris eu turpis. Nulla aliquet. Proin velit. Sed','262','4');
-		('massa','Proin vel nisl. Quisque fringilla euismod enim. Etiam gravida molestie','438','4');
-		('pede.','aliquet diam. Sed diam lorem,','101','2');
-		('sed','placerat. Cras dictum ultricies ligula. Nullam enim. Sed nulla','354','2');
-		('risus.','vitae erat vel pede blandit congue.','455','3');
-		('nec','orci lobortis augue scelerisque mollis. Phasellus libero mauris, aliquam','36','2');
-		('Vivamus','dictum. Proin eget odio. Aliquam vulputate ullamcorper magna.','53','2');
-		('accumsan','Donec egestas. Aliquam nec enim. Nunc ut erat. Sed','217','3');
-		('dictum','porttitor tellus non magna. Nam ligula elit, pretium et,','78','4');
-		('in','auctor, nunc nulla vulputate dui, nec tempus','171','2');
-		('metus','adipiscing non, luctus sit amet, faucibus','257','2');
-		('a','tempor lorem, eget mollis lectus pede','441','2');
-		('augue','egestas. Sed pharetra, felis eget varius ultrices, mauris','151','3');
-		('posuere','Maecenas iaculis aliquet diam. Sed diam','27','3');
-		('vulputate,','metus. Vivamus euismod urna. Nullam lobortis quam a felis','261','4');
-		('nibh.','sed leo. Cras vehicula aliquet libero.','29','3');
-		('non,','et netus et malesuada fames ac turpis egestas. Aliquam','22','2');
-		('tellus,','tellus. Aenean egestas hendrerit neque. In ornare sagittis felis. Donec','435','4');
-		('Nam','pede, ultrices a, auctor non,','276','2');
-		('ut','nisi. Cum sociis natoque penatibus et magnis','173','3');
-		('amet','natoque penatibus et magnis dis parturient','246','4');
-		('egestas.','iaculis odio. Nam interdum enim non nisi. Aenean eget','349','4');
-		('ac','ullamcorper. Duis cursus, diam at pretium','289','2');
-		('ante','sem, vitae aliquam eros turpis non enim. Mauris','212','3');
-		('eget','a purus. Duis elementum, dui','104','4');
-		('pellentesque,','aliquam eros turpis non enim.','160','3');
-		('orci','dolor. Nulla semper tellus id nunc interdum','497','4');
-		('eleifend','nascetur ridiculus mus. Donec dignissim magna','317','4');
-		('semper,','ornare. Fusce mollis. Duis sit amet diam','63','4');
-		('Sed','accumsan convallis, ante lectus convallis','344','3');
-		('tincidunt','non magna. Nam ligula elit, pretium et, rutrum','174','4');
-		('luctus','gravida. Aliquam tincidunt, nunc ac','206','3');
-		('Phasellus','Nam nulla magna, malesuada vel,','98','2');
-		('dictum','nascetur ridiculus mus. Proin vel arcu eu odio tristique','235','3');
-		('sit','commodo auctor velit. Aliquam nisl. Nulla eu neque pellentesque massa','270','2');
-		('lobortis','Vivamus sit amet risus. Donec','42','4');
+INSERT INTO `produits` (`ìd`,`libelle`,`description`,`prix`,`categories_id`) 
+VALUES 	(1,'et,','elit erat vitae risus. Duis a mi fringilla','438','2'),
+		(2,'luctus','Etiam gravida molestie arcu. Sed eu','100','3'),
+		(3,'quis,','feugiat. Sed nec metus facilisis lorem','379','4'),
+		(4,'ligula','posuere vulputate, lacus. Cras interdum. Nunc','394','2'),
+		(5,'Nam','Nullam velit dui, semper et,','431','2'),
+		(6,'bibendum','ullamcorper. Duis cursus, diam at pretium aliquet, metus urna convallis','500','2'),
+		(7,'ipsum.','Suspendisse sagittis. Nullam vitae diam. Proin dolor. Nulla','496','3'),
+		(8,'consectetuer','cubilia Curae; Donec tincidunt. Donec vitae erat','271','3'),
+		(9,'fringilla','tortor at risus. Nunc ac sem','87','4'),
+		(10,'blandit','adipiscing lobortis risus. In mi pede, nonummy ut, molestie','342','3'),
+		(11,'Maecenas','tellus faucibus leo, in lobortis tellus justo','369','2'),
+		(12,'nec','lacus. Mauris non dui nec urna suscipit','152','3'),
+		(13,'turpis.','Proin mi. Aliquam gravida mauris ut mi.','85','3'),
+		(14,'vestibulum,','ipsum nunc id enim. Curabitur','90','4'),
+		(15,'arcu.','fringilla cursus purus. Nullam scelerisque neque sed sem','229','4'),
+		(16,'ipsum','non, luctus sit amet, faucibus ut,','493','4'),
+		(17,'Sed','Sed id risus quis diam luctus lobortis.','289','2'),
+		(18,'risus.','semper, dui lectus rutrum urna, nec','366','3'),
+		(19,'risus','ullamcorper. Duis at lacus. Quisque','427','2'),
+		(20,'egestas','quis massa. Mauris vestibulum, neque sed','203','3'),
+		(21,'ullamcorper','velit eget laoreet posuere, enim nisl elementum purus, accumsan interdum','487','3'),
+		(22,'Morbi','mattis. Cras eget nisi dictum augue malesuada malesuada. Integer','87','4'),
+		(23,'Phasellus','odio. Aliquam vulputate ullamcorper magna.','341','4'),
+		(24,'urna','velit justo nec ante. Maecenas mi','360','4'),
+		(25,'convallis','dolor. Quisque tincidunt pede ac urna. Ut tincidunt','243','3'),
+		(26,'Morbi','turpis. In condimentum. Donec at arcu. Vestibulum ante ipsum primis','367','3'),
+		(27,'purus.','risus. Donec egestas. Duis ac arcu. Nunc mauris. Morbi non','91','4'),
+		(28,'mus.','eros non enim commodo hendrerit. Donec porttitor tellus non magna.','111','2'),
+		(29,'diam.','turpis vitae purus gravida sagittis. Duis gravida. Praesent','218','2'),
+		(30,'et','sagittis lobortis mauris. Suspendisse aliquet molestie tellus. Aenean','393','3'),
+		(31,'nisl.','Suspendisse dui. Fusce diam nunc, ullamcorper','214','4'),
+		(32,'ut','Cum sociis natoque penatibus et magnis dis parturient montes,','71','3'),
+		(33,'eget','molestie pharetra nibh. Aliquam ornare, libero','163','3'),
+		(34,'est,','Proin vel arcu eu odio tristique pharetra. Quisque ac libero','342','2'),
+		(35,'eleifend','pellentesque, tellus sem mollis dui, in sodales elit erat vitae','267','3'),
+		(36,'ipsum','quam quis diam. Pellentesque habitant morbi tristique','292','2'),
+		(37,'dolor','turpis. Nulla aliquet. Proin velit. Sed malesuada augue ut lacus.','193','3'),
+		(38,'vulputate','sit amet risus. Donec egestas. Aliquam nec enim. Nunc','400','4'),
+		(39,'nec,','dictum. Phasellus in felis. Nulla tempor augue ac ipsum.','417','2'),
+		(40,'dis','ut erat. Sed nunc est, mollis non, cursus non, egestas','491','3'),
+		(41,'ornare.','est ac facilisis facilisis, magna tellus faucibus','42','4'),
+		(42,'lacus.','at, velit. Cras lorem lorem,','46','4'),
+		(43,'lacus.','est. Nunc ullamcorper, velit in aliquet lobortis,','400','4'),
+		(44,'aliquam','dolor, tempus non, lacinia at, iaculis quis, pede. Praesent eu','97','4'),
+		(45,'dictum','eu nibh vulputate mauris sagittis placerat. Cras dictum ultricies','365','3'),
+		(46,'vel','lobortis mauris. Suspendisse aliquet molestie tellus. Aenean','391','3'),
+		(47,'ipsum','in aliquet lobortis, nisi nibh','350','2'),
+		(48,'Nam','faucibus. Morbi vehicula. Pellentesque tincidunt tempus risus. Donec egestas.','413','4'),
+		(49,'Ut','risus. Donec egestas. Duis ac arcu.','357','2'),
+		(50,'enim.','ac mattis ornare, lectus ante dictum mi, ac mattis','394','4'),
+		(51,'sapien.','mus. Donec dignissim magna a tortor.','92','2'),
+		(52,'Phasellus','sed leo. Cras vehicula aliquet libero. Integer in magna. Phasellus','361','3'),
+		(53,'lorem','tempus, lorem fringilla ornare placerat, orci lacus vestibulum lorem,','240','3'),
+		(54,'Lorem','Nulla facilisi. Sed neque. Sed','37','4'),
+		(55,'accumsan','nisi. Aenean eget metus. In','144','4'),
+		(56,'nunc','Integer id magna et ipsum cursus vestibulum.','25','2'),
+		(57,'enim.','et magnis dis parturient montes, nascetur ridiculus mus.','114','4'),
+		(58,'Duis','quam. Curabitur vel lectus. Cum sociis natoque','491','2'),
+		(59,'iaculis','libero. Proin mi. Aliquam gravida mauris ut mi. Duis','481','2'),
+		(60,'molestie','taciti sociosqu ad litora torquent per conubia nostra, per','348','4'),
+		(61,'lorem','neque sed sem egestas blandit. Nam nulla magna, malesuada','115','4'),
+		(62,'sapien.','erat volutpat. Nulla dignissim. Maecenas ornare','105','3'),
+		(63,'id,','sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum','59','4'),
+		(64,'egestas.','dignissim. Maecenas ornare egestas ligula. Nullam feugiat placerat','269','3'),
+		(65,'non,','est. Mauris eu turpis. Nulla aliquet. Proin velit. Sed','262','4'),
+		(66,'massa','Proin vel nisl. Quisque fringilla euismod enim. Etiam gravida molestie','438','4'),
+		(67,'pede.','aliquet diam. Sed diam lorem,','101','2'),
+		(68,'sed','placerat. Cras dictum ultricies ligula. Nullam enim. Sed nulla','354','2'),
+		(69,'risus.','vitae erat vel pede blandit congue.','455','3'),
+		(70,'nec','orci lobortis augue scelerisque mollis. Phasellus libero mauris, aliquam','36','2'),
+		(71,'Vivamus','dictum. Proin eget odio. Aliquam vulputate ullamcorper magna.','53','2'),
+		(72,'accumsan','Donec egestas. Aliquam nec enim. Nunc ut erat. Sed','217','3'),
+		(73,'dictum','porttitor tellus non magna. Nam ligula elit, pretium et,','78','4'),
+		(74,'in','auctor, nunc nulla vulputate dui, nec tempus','171','2'),
+		(75,'metus','adipiscing non, luctus sit amet, faucibus','257','2'),
+		(76,'a','tempor lorem, eget mollis lectus pede','441','2'),
+		(77,'augue','egestas. Sed pharetra, felis eget varius ultrices, mauris','151','3'),
+		(78,'posuere','Maecenas iaculis aliquet diam. Sed diam','27','3'),
+		(79,'vulputate,','metus. Vivamus euismod urna. Nullam lobortis quam a felis','261','4'),
+		(80,'nibh.','sed leo. Cras vehicula aliquet libero.','29','3'),
+		(81,'non,','et netus et malesuada fames ac turpis egestas. Aliquam','22','2'),
+		(82,'tellus,','tellus. Aenean egestas hendrerit neque. In ornare sagittis felis. Donec','435','4'),
+		(83,'Nam','pede, ultrices a, auctor non,','276','2'),
+		(84,'ut','nisi. Cum sociis natoque penatibus et magnis','173','3'),
+		(85,'amet','natoque penatibus et magnis dis parturient','246','4'),
+		(86,'egestas.','iaculis odio. Nam interdum enim non nisi. Aenean eget','349','4'),
+		(87,'ac','ullamcorper. Duis cursus, diam at pretium','289','2'),
+		(88,'ante','sem, vitae aliquam eros turpis non enim. Mauris','212','3'),
+		(89,'eget','a purus. Duis elementum, dui','104','4'),
+		(90,'pellentesque,','aliquam eros turpis non enim.','160','3'),
+		(91,'orci','dolor. Nulla semper tellus id nunc interdum','497','4'),
+		(92,'eleifend','nascetur ridiculus mus. Donec dignissim magna','317','4'),
+		(93,'semper,','ornare. Fusce mollis. Duis sit amet diam','63','4'),
+		(94,'Sed','accumsan convallis, ante lectus convallis','344','3'),
+		(95,'tincidunt','non magna. Nam ligula elit, pretium et, rutrum','174','4'),
+		(96,'luctus','gravida. Aliquam tincidunt, nunc ac','206','3'),
+		(97,'Phasellus','Nam nulla magna, malesuada vel,','98','2'),
+		(98,'dictum','nascetur ridiculus mus. Proin vel arcu eu odio tristique','235','3'),
+		(99,'sit','commodo auctor velit. Aliquam nisl. Nulla eu neque pellentesque massa','270','2'),
+		(100,'lobortis','Vivamus sit amet risus. Donec','42','4');
