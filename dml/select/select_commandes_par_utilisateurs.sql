71f19d78fdda670d0cbf67de8acfc5c44d501b77
romaindso <romain.dso@gmail.com>
2012-03-18 20:43:24 +0100

    select commandes par utilisateurs

---
+++ dml/select/select_commandes_par_utilisateurs.sql
@@ -0,0 +1,4 @@
+SELECT u.id, u.nom, u.prenom, cde.id FROM commandes cde 
+INNER JOIN adresses adr ON adr.id = cde.adresses_facturation_id
+INNER JOIN utilisateurs u ON u.id = adr.utilisateurs_id
+GROUP BY u.id
