e56c3ebb001894eb29b3c28b6a8108df181e3fd6
romaindso <romain.dso@gmail.com>
2012-03-20 22:15:34 +0100

    ajout de l'ordre update - proc gestion adresse utilisateur

--- dml/proc/proc_gestion_adresses_utilisateur.sql
+++ dml/proc/proc_gestion_adresses_utilisateur.sql
@@ -6,9 +6,11 @@ BEGIN
     CASE ordre
     WHEN 'add' THEN INSERT INTO adresses (numero, voie, cp, ville, utilisateurs_id, pays_id) VALUES(numero, voie, cp, ville, utilisateur_id, pays_id);
     
-    WHEN 'delete' THEN DELETE FROM adresses WHERE id = adresse_id;
+    WHEN 'del' THEN DELETE FROM adresses WHERE id = adresse_id;
+
+    WHEN 'edit' THEN UPDATE adresses SET numero = numero, voie = voie, cp = cp, ville = ville, pays_id = pays_id WHERE id = adresse_id;
     
-    WHEN 'get' THEN SELECT adr.id, adr.numero, adr.voie, adr.cp, adr.ville, u.id FROM adresses adr 
+    WHEN 'get' THEN SELECT adr.id, adr.numero, adr.voie, adr.cp, adr.ville, adr.pays_id, u.id FROM adresses adr 
                     INNER JOIN utilisateurs u ON u.id = adr.utilisateurs_id
                     WHERE u.id = utilisateur_id;
                             
@@ -17,6 +19,7 @@ BEGIN
 END|
 
 /*CALL gestion_adresses_utilisateur(3, 'get', null, null, null, null, null)|
-CALL gestion_adresses_utilisateur(6, 'add', null, '55', 'Les Champs Elysées', '75000', 'Paris', 1)|*/
-CALL gestion_adresses_utilisateur(6, 'get', null, null, null, null, null, null)|
-/*CALL gestion_adresses_utilisateur(6, 'delete', 63, null, null, null, null, null)|*/
+CALL gestion_adresses_utilisateur(5, 'add', null, '22', 'Rue du loup', '43200', 'Merignac', 1)|
+CALL gestion_adresses_utilisateur(6, 'edit', 64, 50, 'Les champs', '78500', 'Paris', 1)|
+CALL gestion_adresses_utilisateur(null, 'del', 22, null, null, null, null, null)|
+CALL gestion_adresses_utilisateur(6, 'get', null, null, null, null, null, null)|*/
