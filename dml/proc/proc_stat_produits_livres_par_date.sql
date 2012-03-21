75c5b35d3f341ca336d8a9de1d9f3dbd17e6085e
romaindso <romain.dso@gmail.com>
2012-03-21 14:36:04 +0100

    renomage de la proc commande livree par date en stats produit livre par date

--- dml/proc/proc_stat_produits_livres_par_date.sql
+++ dml/proc/proc_stat_produits_livres_par_date.sql
@@ -1,7 +1,7 @@
 DELIMITER |
-DROP PROCEDURE IF EXISTS stat_produits_livrees_par_date|
+DROP PROCEDURE IF EXISTS stat_produits_livres_par_date|
 
-CREATE PROCEDURE stat_produits_livrees_par_date(IN date_inf DATETIME, IN date_sup DATETIME)
+CREATE PROCEDURE stat_produits_livres_par_date(IN date_inf DATETIME, IN date_sup DATETIME)
 BEGIN
 	SELECT count(p.id) AS nombre_de_produits, p.id AS produit_id FROM produits p
 	INNER JOIN livraisons_has_produits lhp ON p.id = lhp.produits_id
@@ -11,5 +11,5 @@ BEGIN
 	ORDER BY nombre_de_produits DESC, produit_id;
 END|
 
-CALL stat_produits_livrees_par_date('2011-12-02', NOW())|
-CALL stat_produits_livrees_par_date('1995-12-02', '2012-01-01')|
+CALL stat_produits_livres_par_date('2011-12-02', NOW())|
+CALL stat_produits_livres_par_date('1995-12-02', '2012-01-01')|
