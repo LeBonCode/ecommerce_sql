b9156a3f3cbec7ef503e8562fe64228bdfefde4b
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-03-21 12:12:47 +0100

    proc gestion_coordonnes_bancaires

--- dml/proc/proc_proc_gestion_coordonnees_bancaires.sql
+++ dml/proc/proc_proc_gestion_coordonnees_bancaires.sql
@@ -1,25 +1,24 @@
 DELIMITER |
 DROP PROCEDURE IF EXISTS gestion_coordonnees_bancaires|
 
-CREATE PROCEDURE gestion_coordonnees_bancaires(IN utilisateur_id INT, IN ordre VARCHAR(100), IN adresse_id INT, IN numero VARCHAR(45), IN voie VARCHAR(45), IN cp VARCHAR(45), IN ville VARCHAR(45), IN pays_id INT)
+CREATE PROCEDURE gestion_coordonnees_bancaires(IN ordre VARCHAR(4), IN numero VARCHAR(16), IN date_expiration DATETIME, IN code_securite INT, IN utilisateur_id INT, IN nom_titulaire VARCHAR(45), IN carte_credit_id INT)
 BEGIN
 	CASE ordre
-		WHEN 'add' THEN INSERT INTO cartes_credit (numero, voie, cp, ville, utilisateurs_id, pays_id) VALUES(numero, voie, cp, ville, utilisateur_id, pays_id);
+		WHEN 'add' THEN INSERT INTO cartes_credit (numero, date_expiration, code_securite, utilisateurs_id, nom_titulaire) VALUES(numero, date_expiration, code_securite, utilisateur_id, nom_titulaire);
 
-		WHEN 'del' THEN DELETE FROM cartes_credit WHERE id = adresse_id;
+		WHEN 'del' THEN DELETE FROM cartes_credit WHERE id = carte_credit_id;
 
-		WHEN 'edit' THEN UPDATE cartes_credit SET numero = numero, voie = voie, cp = cp, ville = ville, pays_id = pays_id WHERE id = adresse_id;
+		WHEN 'edit' THEN UPDATE cartes_credit SET numero = numero, date_expiration = date_expiration, code_securite = code_securite, utilisateurs_id = utilisateur_id, nom_titulaire = nom_titulaire WHERE id = carte_credit_id;
 
-		WHEN 'get' THEN SELECT adr.id, adr.numero, adr.voie, adr.cp, adr.ville, adr.pays_id, u.id FROM cartes_credit cc 
-						INNER JOIN utilisateurs u ON u.id = adr.utilisateurs_id
-						WHERE u.id = utilisateur_id;
+		WHEN 'get' THEN SELECT * FROM cartes_credit WHERE utilisateurs_id = utilisateur_id;
 
 		ELSE SELECT 'Erreur';
     END CASE;
 END|
 
-/*CALL gestion_adresses_utilisateur(3, 'get', null, null, null, null, null)|
-CALL gestion_adresses_utilisateur(5, 'add', null, '22', 'Rue du loup', '43200', 'Merignac', 1)|
-CALL gestion_adresses_utilisateur(6, 'edit', 64, 50, 'Les champs', '78500', 'Paris', 1)|
-CALL gestion_adresses_utilisateur(null, 'del', 22, null, null, null, null, null)|
-CALL gestion_adresses_utilisateur(6, 'get', null, null, null, null, null, null)|*/
+CALL gestion_coordonnees_bancaires("add", 1234567891011121, '2012-12-12', 911, 1, "Lorem Ipsum", null)|
+CALL gestion_coordonnees_bancaires("get", null, null, null, 1, null, null)|
+CALL gestion_coordonnees_bancaires("edit", 1234567891011121, '2012-12-12', 923, 1, "Lorem Ipsum", last_insert_id())|
+CALL gestion_coordonnees_bancaires("get", null, null, null, 1, null, null)|
+CALL gestion_coordonnees_bancaires("del", null, null, null, null, null, last_insert_id())|
+CALL gestion_coordonnees_bancaires("get", null, null, null, 1, null, null)|
