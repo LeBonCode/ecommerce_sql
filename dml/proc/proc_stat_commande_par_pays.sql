b360cfa20203671b154a0c3a1275631419307a3a
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-03-21 16:13:20 +0100

    curseur 2

--- dml/proc/proc_stat_commande_par_pays.sql
+++ dml/proc/proc_stat_commande_par_pays.sql
@@ -1,17 +1,37 @@
+/* ATTENTION genere un fichier csv par pays dans le dossier mydb(repertoire de la base de données) qui est dans mysql */
+/* Il faut penser à les supprimer après tests */
 DELIMITER |
 DROP PROCEDURE IF EXISTS stat_commande_par_pays|
 
 CREATE PROCEDURE stat_commande_par_pays(IN date_inf DATETIME, IN date_sup DATETIME)
 BEGIN
+	DECLARE done INT DEFAULT 0;
+	DECLARE pays_id INT;
+	DECLARE nom_pays VARCHAR(45);
 	DECLARE curseur CURSOR FOR SELECT * FROM pays;
-	OPEN curseur1; # ouverture du curseur1
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
-    # première ligne du résultat
-    FETCH curseur1 INTO var_identifiant, var_mot_passe;
-    SELECT var_identifiant, var_mot_passe;
-
-    CLOSE curseur1; # fermeture du curseur1
+	OPEN curseur;
+	REPEAT
+	FETCH curseur INTO pays_id, nom_pays;
+	SET @sql_text = 
+	CONCAT (
+		"SELECT * 
+		FROM commandes c
+		INNER JOIN adresses a ON a.id = c.adresses_facturation_id
+		WHERE a.pays_id = pays_id
+		INTO OUTFILE 'commandes_",
+		nom_pays,
+		date_format(now(),'%Y%m%d%H%i'),
+		".csv' ",
+		"FIELDS TERMINATED BY ','
+		ENCLOSED BY '\"'
+		LINES TERMINATED BY '\n'"
+	);
+	PREPARE s1 FROM @sql_text;
+	EXECUTE s1;
+	UNTIL done END REPEAT;
 END|
 
-CALL stat_produits_livrees_par_date('2011-12-02', NOW())|
-CALL stat_produits_livrees_par_date('1995-12-02', '2012-01-01')|
+CALL stat_commande_par_pays('2011-12-02', NOW())|
+/*CALL stat_commande_par_pays('1995-12-02', '2012-01-01')|*/
