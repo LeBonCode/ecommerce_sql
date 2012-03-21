a66aa0b56232f22cc9b7f4b20f5e81d375e17e5c
romaindso <romain.dso@gmail.com>
2012-03-21 11:25:04 +0100

    rajout d'un order by date pour la liste des commandes d'un utilisateur

--- dml/proc/proc_list_commandes_utilisateur.sql
+++ dml/proc/proc_list_commandes_utilisateur.sql
@@ -3,10 +3,11 @@ DROP PROCEDURE IF EXISTS list_commandes_utilisateur|
 
 CREATE PROCEDURE list_commandes_utilisateur(IN utilisateur_id INT)
 BEGIN
-    SELECT u.id, u.nom, u.prenom, cde.id FROM commandes cde 
+    SELECT u.id AS utilisateur_id, u.nom, u.prenom, cde.id AS commande_id, cde.date FROM commandes cde 
     INNER JOIN adresses adr ON adr.id = cde.adresses_facturation_id
     INNER JOIN utilisateurs u ON u.id = adr.utilisateurs_id
-    WHERE u.id = utilisateur_id;
+    WHERE u.id = utilisateur_id 
+    ORDER BY cde.date;
 END|
 
 CALL list_commandes_utilisateur(5)|
