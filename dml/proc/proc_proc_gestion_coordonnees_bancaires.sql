5855cbfce019d13b9a8778cd8be4d9712e848de6
Quentin Decoudier <quentin.decoudier@gmail.com>
2012-03-21 12:05:11 +0100

    proc gestion_coordonnees_bancaires

---
+++ dml/proc/proc_proc_gestion_coordonnees_bancaires.sql
@@ -0,0 +1,25 @@
+DELIMITER |
+DROP PROCEDURE IF EXISTS gestion_coordonnees_bancaires|
+
+CREATE PROCEDURE gestion_coordonnees_bancaires(IN utilisateur_id INT, IN ordre VARCHAR(100), IN adresse_id INT, IN numero VARCHAR(45), IN voie VARCHAR(45), IN cp VARCHAR(45), IN ville VARCHAR(45), IN pays_id INT)
+BEGIN
+	CASE ordre
+		WHEN 'add' THEN INSERT INTO cartes_credit (numero, voie, cp, ville, utilisateurs_id, pays_id) VALUES(numero, voie, cp, ville, utilisateur_id, pays_id);
+
+		WHEN 'del' THEN DELETE FROM cartes_credit WHERE id = adresse_id;
+
+		WHEN 'edit' THEN UPDATE cartes_credit SET numero = numero, voie = voie, cp = cp, ville = ville, pays_id = pays_id WHERE id = adresse_id;
+
+		WHEN 'get' THEN SELECT adr.id, adr.numero, adr.voie, adr.cp, adr.ville, adr.pays_id, u.id FROM cartes_credit cc 
+						INNER JOIN utilisateurs u ON u.id = adr.utilisateurs_id
+						WHERE u.id = utilisateur_id;
+
+		ELSE SELECT 'Erreur';
+    END CASE;
+END|
+
+/*CALL gestion_adresses_utilisateur(3, 'get', null, null, null, null, null)|
+CALL gestion_adresses_utilisateur(5, 'add', null, '22', 'Rue du loup', '43200', 'Merignac', 1)|
+CALL gestion_adresses_utilisateur(6, 'edit', 64, 50, 'Les champs', '78500', 'Paris', 1)|
+CALL gestion_adresses_utilisateur(null, 'del', 22, null, null, null, null, null)|
+CALL gestion_adresses_utilisateur(6, 'get', null, null, null, null, null, null)|*/
